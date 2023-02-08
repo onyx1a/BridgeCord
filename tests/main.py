ebdb878489c2dee64a98ac7a2e14a34c3bdbda7a
onyx1a <onyx1anis@gmail.com>
2023-02-08 17:04:43 +0200

    Skip test on Win platform

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,7 @@
-import DiscordBridge
+import sys
+import pytest
 
+@pytest.mark.skipif(sys.platform.startswith("win32"), reason="NYI")
 def test_main():
+    import DiscordBridge
     assert DiscordBridge.TestBridge() == "Make Bridge!"
